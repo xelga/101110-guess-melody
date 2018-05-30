94b038f4dede0e4b5bc31abc1b0fe4feeb72bc73
Xelga <wanxelga@yandex.ru>
2018-05-30 17:45:11 +0300

    Добавлена логика для переключения экранов;

---
+++ js/main.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const SCREEN_DEFAULT_NUMBER = 0;
+const ARROW_LEFT_KEYCODE = 37;
+const ARROW_RIGHT_KEYCODE = 39;
+const ARROWS_TEMPLATE = `<div class="arrows__wrap">
+    <style>
+      .arrows__wrap {
+        position: absolute;
+        top: 135px;
+        left: 50%;
+        margin-left: -56px;
+      }
+      .arrows__btn {
+        background: none;
+        border: 2px solid black;
+        padding: 5px 20px;
+      }
+    </style>
+    <button class="arrows__btn"><-</button>
+    <button class="arrows__btn">-></button>
+</div>`;
+
+const templates = document.querySelector(`#templates`).content.querySelectorAll(`.main`);
+const screens = [];
+const app = document.querySelector(`.app`);
+const main = app.querySelector(`.main`);
+
+app.insertAdjacentHTML(`beforeend`, ARROWS_TEMPLATE);
+
+const arrowsButton = app.querySelectorAll(`.arrows__btn`);
+const arrowLeft = arrowsButton[0];
+const arrowRight = arrowsButton[1];
+
+for (let i = 0; i < templates.length; i++) {
+  screens.push(templates[i]);
+}
+
+const showScreen = (number) => {
+  main.innerHTML = screens[number].innerHTML;
+  main.className = screens[number].className;
+  return number;
+};
+
+let currentScreen = showScreen(SCREEN_DEFAULT_NUMBER);
+
+document.addEventListener(`keydown`, function (event) {
+  if (event.keyCode === ARROW_LEFT_KEYCODE && currentScreen > 0 && currentScreen < screens.length) {
+    currentScreen = showScreen(currentScreen - 1);
+  }
+
+  if (event.keyCode === ARROW_RIGHT_KEYCODE && currentScreen >= 0 && currentScreen < screens.length - 1) {
+    currentScreen = showScreen(currentScreen + 1);
+  }
+});
+
+arrowLeft.addEventListener(`click`, function () {
+  if (currentScreen > 0 && currentScreen < screens.length) {
+    currentScreen = showScreen(currentScreen - 1);
+  }
+});
+
+arrowRight.addEventListener(`click`, function () {
+  if (currentScreen >= 0 && currentScreen < screens.length - 1) {
+    currentScreen = showScreen(currentScreen + 1);
+  }
+});
