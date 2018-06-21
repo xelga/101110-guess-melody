50e75d4da0e0e3111d6f16c184d807b14a575d00
Xelga <wanxelga@yandex.ru>
2018-06-21 22:58:14 +0300

    Модуль 6, исправлен template для таймера;

--- js/game-screen-view.js
+++ js/game-screen-view.js
@@ -17,9 +17,8 @@ export default class GameScreenView extends AbstractView {
     <svg xmlns="http://www.w3.org/2000/svg" class="timer" viewBox="0 0 780 780">
       <circle cx="390" cy="390" r="370" class="timer-line"
         style="filter: url(.#blur); transform: rotate(-90deg) scaleY(-1); transform-origin: center"></circle>
-        <div class="timer-value" xmlns="http://www.w3.org/1999/xhtml">
-        </div>
     </svg>
+    <div class="timer-value"></div>
     <div class="main-mistakes">
         ${new Array(this.gameConfig.lives - this.gameState.lives).fill(`<img class="main-mistake" src="img/wrong-answer.png" width="35" height="49">`).join(``)}
     </div>
